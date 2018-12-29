49c086cc04f16fc9420c6116cf5145942a107fbd
Jim_C_Morri <james.coffey@morrisonsplc.co.uk>
2018-12-29 18:41:54 +0000

    splitting routes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,9 +6,10 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { AuthRoutes } from './auth-routing.module';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [CommonModule, ReactiveFormsModule, AngularFireAuthModule, SharedModule]
+  imports: [CommonModule, ReactiveFormsModule, AngularFireAuthModule, SharedModule, AuthRoutes]
 })
 export class AuthModule {}
