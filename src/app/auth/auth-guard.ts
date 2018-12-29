2f2360b022e4fb4461c5b9806a56afc458052156
Jim_C_Morri <james.coffey@morrisonsplc.co.uk>
2018-12-29 19:27:34 +0000

    lazy auth guard

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,14 +1,22 @@
-import { Injectable, OnInit, OnDestroy } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Router, Route, CanActivate, CanLoad, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthService } from './auth.service';
 import { UIService } from '../shared/ui.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router, private uiService: UIService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  canLoad(route: Route) {
     if (this.authService.isAuth()) {
       return true;
     } else {
